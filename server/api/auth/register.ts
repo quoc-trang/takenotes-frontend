4d09078d304c5da43121d8f7feb79e5211a472c0
quoctrang <quoctrang2701@gmail.com>
2026-01-15 22:19:03 +0700

    fix type

--- server/api/auth/register.ts
+++ server/api/auth/register.ts
@@ -1,4 +1,4 @@
-export default eventHandler(async (event) => {
+export default eventHandler(async (event): Promise<any> => {
   try {
     const body = await readBody(event)
     const config = useRuntimeConfig()
